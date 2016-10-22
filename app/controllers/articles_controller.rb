46dba754dff50017f87d11cf46185c8d25f7b11d
hirokishingu <hiron@hiron-no-Mac-mini-2.local>
2016-10-22 17:46:00 +0900

    perfectadmin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = "自身の記事のみ編集または削除できます。"
       redirect_to root_path
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.all
   end
 
   def new
-  @user = User.new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "ようこそ! BRAND へ #{@user.username}"
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -38,19 +40,35 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "ユーザーと記事が削除されました。"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
   def set_user
     @user = User.find(params[:id])
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "自身のアカウントのみ編集できます。"
       redirect_to root_path
     end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "管理者のみが実行できます。"
+      redirect_to root_path
+    end
+  end
 
 end
 

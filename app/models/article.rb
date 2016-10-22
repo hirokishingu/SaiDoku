b3a6731a6b5d2c0cf07cc8da93506ec8468318f1
hirokishingu <hiron@hiron-no-Mac-mini-2.local>
2016-10-23 07:47:40 +0900

    カテゴリー改良

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 120 }
   validates :user_id, presence: true
